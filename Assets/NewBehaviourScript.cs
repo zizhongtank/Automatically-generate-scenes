97cc8f67e36781afabce3c42b9f4cd7892edc324
zizhongtank <32141317+zizhongtank@users.noreply.github.com>
2018-03-31 04:56:19 -0400

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject floor;
+    public Sprite[] floorSp;
+    // Use this for initialization
+    void Start()
+    {
+       
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            GameObject floor0 = (GameObject)Instantiate(floor, new Vector3(0.32f * i, 0.32f * j, 0), Quaternion.identity);
+                            floor.GetComponent<SpriteRenderer>().sprite = floorSp[Random.Range(0, floorSp.Length - 1)];
+                        }
+                    }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
